bb71a52d6976391081b4e1fd30126379c42475de
poly <jun1234755@naver.com>
2024-11-06 11:06:11 +0900

    2024-11-06

---
+++ ApiGateWay/src/main/java/kopo/poly/ApiGateWayApplication.java
@@ -0,0 +1,17 @@
+package kopo.poly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@EnableWebFlux
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ApiGateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGateWayApplication.class, args);
+    }
+
+}
